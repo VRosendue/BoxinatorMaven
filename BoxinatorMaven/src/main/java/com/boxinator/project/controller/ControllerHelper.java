92539e797e12d84a51ba7235463ad2f50c15f266
VRosendue <110988416+VRosendue@users.noreply.github.com>
2022-10-14 11:08:18 +0200

    Delete BoxinatorMaven directory

--- BoxinatorMaven/src/main/java/com/boxinator/project/controller/ControllerHelper.java
+++
@@ -1,8 +0,0 @@
-package com.boxinator.project.controller;
-
-public class ControllerHelper {
-
-	
-	public static final String BASE_URI_V1 = "/v1/";
-
-}
--- BoxinatorMaven/src/main/resources/CountryData.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO country VALUES(1,'Germany', 'Denmark', '3670'),
-(2, 'Denmark', 'Germany', '10115'),
-(3, 'Japan', 'Denmark', '看板')
